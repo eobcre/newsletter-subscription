8c604afd6fc25a2ca1aef23c66a9e284d03f8ec4
eobcre <eobcre@gmail.com>
2023-08-22 11:17:34 -0400

    placed components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import Title from './components/Title';
+import CheckContents from './components/CheckContents';
+import EmailInput from './components/EmailInput';
+import SubscribeButton from './components/SubscribeButton';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <Title />
+        <CheckContents />
+        <EmailInput />
+        <SubscribeButton />
+      </div>
+    </div>
+  );
 };
 
 export default App;
