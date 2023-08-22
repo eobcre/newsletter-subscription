b15029a5717ef84b320be73ec459599c28ec41d1
eobcre <eobcre@gmail.com>
2023-08-22 11:36:58 -0400

    created button and styled

--- src/components/SubscribeButton.tsx
+++ src/components/SubscribeButton.tsx
@@ -1,5 +1,11 @@
 const SubscribeButton = () => {
-  return <div></div>;
+  return (
+    <>
+      <button className='bg-[#4A79EF] font-sans text-white text-sm rounded-sm px-8 py-4'>
+        Subscribe
+      </button>
+    </>
+  );
 };
 
 export default SubscribeButton;
