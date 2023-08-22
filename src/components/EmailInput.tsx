cc853015734b83b8cb4975b519078eb8fea83d43
eobcre <eobcre@gmail.com>
2023-08-22 11:42:37 -0400

    created input and styled

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -1,5 +1,13 @@
 const EmailInput = () => {
-  return <div></div>;
+  return (
+    <div>
+      <input
+        type='text'
+        placeholder='Enter your email address'
+        className='border rounded-sm outline-none px-3 py-4 w-[360px]'
+      />
+    </div>
+  );
 };
 
 export default EmailInput;
