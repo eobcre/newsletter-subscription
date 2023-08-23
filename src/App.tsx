33175ccd2cea84b52ceeaf3c1eec3da02047c710
eobcre <eobcre@gmail.com>
2023-08-23 11:03:32 -0400

    applied component type and updated style

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import CheckContents from './components/CheckContents';
 import EmailInput from './components/EmailInput';
 import SubscribeButton from './components/SubscribeButton';
 
-const App = () => {
+const App: React.FC = () => {
   return (
     <div className='bg-[#DDE1FD] h-screen'>
       <div className='flex justify-center items-center h-screen'>
-        <div className='bg-white rounded-md w-[680px] h-[320px]'>
+        <div className='bg-white rounded-md w-[710px] h-[320px]'>
           <Title />
           <CheckContents />
-          <div className='flex gap-2'>
+          <div className='flex gap-2 px-14 py-7'>
             <EmailInput />
             <SubscribeButton />
           </div>
