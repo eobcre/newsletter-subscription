f63d8c5e8e1f97d57951b41e953ac053e61c2dad
eobcre <eobcre@gmail.com>
2023-08-23 11:01:28 -0400

    applied component type and padding updated

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,6 +1,6 @@
-const Title = () => {
+const Title: React.FC = () => {
   return (
-    <div>
+    <div className='pl-16 py-6'>
       <h1 className='font-sans font-bold'>Sign up for newsletters</h1>
     </div>
   );
