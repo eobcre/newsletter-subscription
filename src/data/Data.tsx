9aea92f57fc8a5730661ae673964c252876eb986
eobcre <eobcre@gmail.com>
2023-08-23 11:03:53 -0400

    created data

---
+++ src/data/Data.tsx
@@ -0,0 +1,26 @@
+export const Data = [
+  {
+    name: 'Daily Bits',
+    sub: "Today's biggest news",
+  },
+  {
+    name: 'The Exchange',
+    sub: 'Art, money and markets',
+  },
+  {
+    name: 'Sponsorship',
+    sub: 'Partnership insights',
+  },
+  {
+    name: 'Week in Review',
+    sub: "The week's biggest news",
+  },
+  {
+    name: 'Event Updates',
+    sub: 'Announcements and discounts',
+  },
+  {
+    name: 'This Week in Art',
+    sub: 'Art news of the week',
+  },
+];
